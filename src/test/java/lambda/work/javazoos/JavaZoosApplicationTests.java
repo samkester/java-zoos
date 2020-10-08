a8d68f908fb30dd3b14af64a2945c65d31ea3e0c
Sam Kester <samkester@gmail.com>
2020-10-08 16:41:04 -0600

    initial setup

---
+++ src/test/java/lambda/work/javazoos/JavaZoosApplicationTests.java
@@ -0,0 +1,13 @@
+package lambda.work.javazoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaZoosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
